ea55019941aa7b96079641f310f3ed672f812910
TheSSJ <greg.house.ssj@gmail.com>
2019-06-17 12:10:11 +0000

    LPM: use lower power mode a little more
    Change-Id: If8722a07a3b15ac1a867906bb70c957766283e35

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -563,7 +563,7 @@ static int cpu_power_select(struct cpuidle_device *dev,
 
 		lvl_latency_us = pwr_params->latency_us;
 
-		if (latency_us < lvl_latency_us)
+		if (latency_us <= lvl_latency_us)
 			break;
 
 		if (next_event_us) {
@@ -904,7 +904,7 @@ static int cluster_select(struct lpm_cluster *cluster, bool from_idle,
 					&level->num_cpu_votes))
 			continue;
 
-		if (from_idle && latency_us < pwr_params->latency_us)
+		if (from_idle && latency_us <= pwr_params->latency_us)
 			break;
 
 		if (suspend_in_progress && from_idle && level->notify_rpm)
